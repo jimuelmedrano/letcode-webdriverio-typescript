3b75dfb09a4940baff673aa85f77d7fbcbb72e58
Jimuel Renzo Medrano <42688255+mrnjii@users.noreply.github.com>
2023-07-11 20:12:10 +0800

    Create logger.ts

---
+++ logger.ts
@@ -0,0 +1,19 @@
+import winston from "winston";
+
+const resultLogger = winston.createLogger({
+    level: 'info',
+    format: winston.format.simple(),
+    transports: [
+        new winston.transports.File({ filename: 'logs/results.log' }),
+    ]
+});
+
+const resultErrorLogger = winston.createLogger({
+    level: 'info',
+    format: winston.format.simple(),
+    transports: [
+        new winston.transports.File({ filename: 'logs/resultsErrors.log' }),
+    ]
+});
+
+export default {resultLogger,resultErrorLogger};
